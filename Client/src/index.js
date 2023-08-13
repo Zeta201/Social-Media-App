e1394f25674cbf7aae1626b58ea93ad029dfda56
Zeta201 <dini2040gh@gmail.com>
2023-08-13 19:26:13 +0530

    env fle

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,20 +3,17 @@ import ReactDom from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "@asgardeo/auth-react";
-import configData from "./config.json";
 
 const config = {
-  signInRedirectURL: configData.REACT_APP_SIGN_IN_REDIRECT_URL,
-  signOutRedirectURL: configData.REACT_APP_SIGN_OUT_REDIRECT_URL,
-  clientID: configData.REACT_APP_CLIENT_ID,
-  baseUrl: configData.REACT_APP_BASE_URL,
+  signInRedirectURL: process.env.REACT_APP_SIGN_IN_REDIRECT_URL,
+  signOutRedirectURL: process.env.REACT_APP_SIGN_OUT_REDIRECT_URL,
+  clientID: process.env.REACT_APP_CLIENT_ID,
+  baseUrl: process.env.REACT_APP_BASE_URL,
   scope: ["openid", "profile"],
 };
-
-console.log(config);
 const el = document.getElementById("root");
 const root = ReactDom.createRoot(el);
-// wow
+console.log(config);
 root.render(
   <AuthProvider config={config}>
     <BrowserRouter>
