eee0303cac345a54934aab4af5105a88c4897d46
Zeta201 <dini2040gh@gmail.com>
2023-08-13 19:56:47 +0530

    edit

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,12 +5,22 @@ import Login from "./Components/Login/Login";
 import { useAuthContext } from "@asgardeo/auth-react";
 
 function App() {
+  const { state } = useAuthContext();
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/" element={<Navigate to="/login" />}></Route>
+        <Route
+          path="/"
+          element={
+            state.isAuthenticated ? (
+              <Navigate to="/dashboard" />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        ></Route>
       </Routes>
     </div>
   );
